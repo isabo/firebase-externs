d97ea244830c030d61b3a3dfa7687f07049cd952
isabo <isabo@claritude.com>
2015-06-09 10:30:16 +0300

    Support the .code property on Firebase errors

--- firebase-externs.js
+++ firebase-externs.js
@@ -7,8 +7,8 @@ var Firebase = function (firebaseURL) {};
 
 /**
  * @param {string} authToken
- * @param {!function(Error, {auth:Object, expires:number})=} onComplete
- * @param {!function(!Error)=} onCancel
+ * @param {!function(Firebase.Error, {auth:Object, expires:number})=} onComplete
+ * @param {!function(!Firebase.Error)=} onCancel
  * @deprecated
  */
 Firebase.prototype.auth = function (authToken, onComplete, onCancel) {};
@@ -16,14 +16,14 @@ Firebase.prototype.auth = function (authToken, onComplete, onCancel) {};
 
 /**
  * @param {string} authToken
- * @param {!function(Error, Firebase.AuthCallbackData)} onComplete
+ * @param {!function(Firebase.Error, Firebase.AuthCallbackData)} onComplete
  * @param {!Object=} options
  */
 Firebase.prototype.authWithCustomToken = function (authToken, onComplete, options) {};
 
 
 /**
- * @param {!function(Error, Firebase.AuthCallbackData)} onComplete
+ * @param {!function(Firebase.Error, Firebase.AuthCallbackData)} onComplete
  * @param {!Object=} options
  */
 Firebase.prototype.authAnonymously = function (onComplete, options) {};
@@ -31,7 +31,7 @@ Firebase.prototype.authAnonymously = function (onComplete, options) {};
 
 /**
  * @param {!Firebase.AuthPasswordCredentials} credentials
- * @param {!function(Error, Firebase.AuthCallbackData)} onComplete
+ * @param {!function(Firebase.Error, Firebase.AuthCallbackData)} onComplete
  * @param {!Object=} options
  */
 Firebase.prototype.authWithPassword = function (credentials, onComplete, options) {};
@@ -39,7 +39,7 @@ Firebase.prototype.authWithPassword = function (credentials, onComplete, options
 
 /**
  * @param {string} provider
- * @param {!function(Error, Firebase.AuthCallbackData)} onComplete
+ * @param {!function(Firebase.Error, Firebase.AuthCallbackData)} onComplete
  * @param {!Object=} options
  */
 Firebase.prototype.authWithOAuthPopup = function (provider, onComplete, options) {};
@@ -47,7 +47,7 @@ Firebase.prototype.authWithOAuthPopup = function (provider, onComplete, options)
 
 /**
  * @param {string} provider
- * @param {!function(!Error)} onComplete
+ * @param {!function(!Firebase.Error)} onComplete
  * @param {!Object=} options
  */
 Firebase.prototype.authWithOAuthRedirect = function (provider, onComplete, options) {};
@@ -56,7 +56,7 @@ Firebase.prototype.authWithOAuthRedirect = function (provider, onComplete, optio
 /**
  * @param {string} provider
  * @param {string|!Object} credentials
- * @param {!function(Error, Firebase.AuthCallbackData)} onComplete
+ * @param {!function(Firebase.Error, Firebase.AuthCallbackData)} onComplete
  * @param {!Object=} options
  */
 Firebase.prototype.authWithOAuthToken = function (provider, credentials, onComplete, options) {};
@@ -135,27 +135,27 @@ Firebase.prototype.toString = function () {};
 
 /**
  * @param {Firebase.Value} value
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.prototype.set = function (value, onComplete) {};
 
 
 /**
  * @param {!Object<string, Firebase.Value>} value
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.prototype.update = function (value, onComplete) {};
 
 
 /**
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.prototype.remove = function (onComplete) {};
 
 
 /**
  * @param {Firebase.Value=} value
- * @param {function(Error)=} onComplete
+ * @param {function(Firebase.Error)=} onComplete
  * @return {!Firebase}
  */
 Firebase.prototype.push = function (value, onComplete) {};
@@ -164,21 +164,21 @@ Firebase.prototype.push = function (value, onComplete) {};
 /**
  * @param {Firebase.Value} value
  * @param {(string|number)} priority
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.prototype.setWithPriority = function (value, priority, onComplete) {};
 
 
 /**
  * @param {(string|number)} priority
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.prototype.setPriority = function (priority, onComplete) {};
 
 
 /**
  * @param {!Firebase.TransactionUpdateFunction} updateFunction
- * @param {!function(Error, boolean, !Firebase.DataSnapshot)=} onComplete
+ * @param {!function(Firebase.Error, boolean, !Firebase.DataSnapshot)=} onComplete
  * @param {boolean=} applyLocally
  */
 Firebase.prototype.transaction = function (updateFunction, onComplete, applyLocally) {};
@@ -186,35 +186,35 @@ Firebase.prototype.transaction = function (updateFunction, onComplete, applyLoca
 
 /**
  * @param {!Firebase.AuthPasswordCredentials} credentials
- * @param {!function(Error, {uid:string})} onComplete
+ * @param {!function(Firebase.Error, {uid:string})} onComplete
  */
 Firebase.prototype.createUser = function (credentials, onComplete) {};
 
 
 /**
  * @param {!{oldEmail:string, password:string, newEmail:string}} credentials
- * @param {!function(Error)} onComplete
+ * @param {!function(Firebase.Error)} onComplete
  */
 Firebase.prototype.changeEmail = function (credentials, onComplete) {};
 
 
 /**
  * @param {!{email:string, oldPassword:string, newPassword:string}} credentials
- * @param {!function(Error)} onComplete
+ * @param {!function(Firebase.Error)} onComplete
  */
 Firebase.prototype.changePassword = function (credentials, onComplete) {};
 
 
 /**
  * @param {!Firebase.AuthPasswordCredentials} credentials
- * @param {!function(Error)} onComplete
+ * @param {!function(Firebase.Error)} onComplete
  */
 Firebase.prototype.removeUser = function (credentials, onComplete) {};
 
 
 /**
  * @param {!{email:string}} credentials
- * @param {!function(Error)} onComplete
+ * @param {!function(Firebase.Error)} onComplete
  */
 Firebase.prototype.resetPassword = function (credentials, onComplete) {};
 
@@ -234,7 +234,7 @@ Firebase.prototype.goOffline = function () {};
 /**
  * @param {string} eventType
  * @param {!Firebase.EventCallback} callback
- * @param {!function(!Error)=} cancelCallback
+ * @param {!function(!Firebase.Error)=} cancelCallback
  * @param {!Object=} context
  * @return {!function(!Firebase.DataSnapshot, ?string=)}
  */
@@ -252,7 +252,7 @@ Firebase.prototype.off = function (eventType, callback, context) {};
 /**
  * @param {string} eventType
  * @param {!Firebase.EventCallback} successCallback
- * @param {!function(!Error)=} failureCallback
+ * @param {!function(!Firebase.Error)=} failureCallback
  * @param {!Object=} context
  */
 Firebase.prototype.once = function (eventType, successCallback, failureCallback, context) {};
@@ -384,20 +384,20 @@ Firebase.OnDisconnect = function () {};
 
 /**
  * @param {Firebase.Value} value
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.OnDisconnect.prototype.set = function (value, onComplete) {};
 
 
 /**
  * @param {!Object<string, Firebase.Value>} value
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.OnDisconnect.prototype.update = function (value, onComplete) {};
 
 
 /**
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.OnDisconnect.prototype.remove = function (onComplete) {};
 
@@ -405,13 +405,13 @@ Firebase.OnDisconnect.prototype.remove = function (onComplete) {};
 /**
  * @param {Firebase.Value} value
  * @param {(string|number)} priority
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.OnDisconnect.prototype.setWithPriority = function (value, priority, onComplete) {};
 
 
 /**
- * @param {!function(Error)=} onComplete
+ * @param {!function(Firebase.Error)=} onComplete
  */
 Firebase.OnDisconnect.prototype.cancel = function (onComplete) {};
 
@@ -551,3 +551,18 @@ Firebase.AuthPasswordCredentials;
  * @typedef {(function(!Firebase.DataSnapshot, ?string)|function(!Firebase.DataSnapshot))}
  */
 Firebase.EventCallback;
+
+
+
+
+/**
+ * @constructor
+ * @extends {Error}
+ */
+Firebase.Error = function () {};
+
+
+/**
+ * @type {string}
+ */
+Firebase.Error.prototype.code;
